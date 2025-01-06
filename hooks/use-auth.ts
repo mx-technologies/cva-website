8f71a26aca266321a3088a24c38ddd86712cf3d3
manuelExtra <Olaleyeemmanuel23@gmail.com>
2025-01-06 02:19:19 +0100

    implement modifications on use-auth middleware

--- hooks/use-auth.ts
+++ hooks/use-auth.ts
@@ -1,11 +1,12 @@
 'use client';
 
 import { useSelector } from 'react-redux';
-import { useRouter } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
 import { useEffect } from 'react';
 
 const useAuth = () => {
   const router = useRouter();
+  const pathname = usePathname();
   const user = useSelector((state: any) => state.auth.user);
 
   useEffect(() => {
@@ -13,7 +14,9 @@ const useAuth = () => {
       // Redirect to login if user is not authenticated
       router.push('/console-signin'); // Adjust the path based on your routing structure
     } else {
-      router.push('/console');
+      if (pathname === '/console-signin') {
+        router.push('/console');
+      }
     }
   });
 };
