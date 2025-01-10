6ea6f809c7e79e6baf1ed5217431eaf5293e0d43
manuelExtra <Olaleyeemmanuel23@gmail.com>
2025-01-10 03:40:02 +0100

    implement fix for command component

---
+++ cmd.d.ts
@@ -0,0 +1,8 @@
+declare module 'cmdk' {
+  import { FC } from 'react';
+
+  export const Command: FC<any>;
+  export const CommandInput: FC<any>;
+  export const CommandList: FC<any>;
+  export const CommandEmpty: FC<any>;
+}
