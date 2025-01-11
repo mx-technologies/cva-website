e62ddc1dbde283274a6d2732fee74f08d31bbba7
manuelExtra <Olaleyeemmanuel23@gmail.com>
2025-01-11 23:16:48 +0100

    implement modifications on our programs section component

--- app/(main)/(landing)/_components/our-programs-section.tsx
+++ app/(main)/(landing)/_components/our-programs-section.tsx
@@ -59,19 +59,18 @@ export default function OurProgramsSection() {
   };
 
   return (
-    <div className='bg-white py-12 md:py-20 pl-4 md:pl-16 lg:pl-32'>
-      <div className='p-2 md:pl-20 ml-auto flex flex-col md:grid md:grid-cols-[1fr_2fr] gap-10 items-center'>
+    <div className='bg-white py-12 md:py-20 px-4 md:px-16 lg:pl-32'>
+      <div className='p-2 lg:pl-20 ml-auto flex flex-col lg:grid lg:grid-cols-[1fr_2fr] gap-10 items-center'>
         {/* Left Section */}
-        <div className='flex gap-3 flex-1 flex-col md:flex-row'>
-          <div className='w-full md:w-72'>
+        <div className='flex gap-3 flex-1 flex-col sm:flex-row md:flex-row'>
+          <div className='w-full lg:w-72'>
             <h2
-              className={`text-center md:text-left text-3xl md:text-5xl font-bold text-gray-900 mb-4 ${playfairDisplay.className}`}
+              className={`flex lg:flex-col justify-center lg:text-left text-3xl md:text-5xl font-bold text-gray-900 mb-4 ${playfairDisplay.className} gap-2 w-full`}
             >
-              Our <br />
-              <span className=''>Programs</span>
+              <span>Our</span> <span>Programs</span>
             </h2>
             <div
-              className={`text-center md:text-left w-full text-gray-700 leading-relaxed ${openSans.className}`}
+              className={`text-center lg:text-left w-full text-gray-700 leading-relaxed ${openSans.className}`}
             >
               Experience God’s presence, connect with a loving community, and
               discover opportunities to deepen your faith and make an impact for
@@ -83,14 +82,14 @@ export default function OurProgramsSection() {
             alt='arrow-diagonal'
             width='150'
             height='50'
-            className='self-center hidden md:block'
+            className='self-center hidden lg:block'
           />
         </div>
 
         {/* Right Section */}
         <div className='flex space-y-4 flex-col md:flex-row gap-10'>
           {/* Main Carousel View */}
-          <div className='border border-primary-main rounded-3xl p-4 w-full md:w-[300px] m-auto'>
+          <div className='flex flex-row lg:flex-col border border-primary-main rounded-3xl p-4 w-full sm:w-full lg:w-[400px] m-auto gap-5 lg:gap-0'>
             <div className='relative rounded-lg mb-4'>
               <Image
                 src={carouselItems[currentIndex].image}
@@ -101,26 +100,28 @@ export default function OurProgramsSection() {
                 className='rounded-lg'
               />
             </div>
-            <h3
-              className={`text-2xl font-bold text-gray-900 mb-2 ${playfairDisplay.className}`}
-            >
-              {carouselItems[currentIndex].title}
-            </h3>
-            <p
-              className={`text-secondary-main leading-relaxed mb-4 text-sm ${openSans.className}`}
-            >
-              {carouselItems[currentIndex].description}
-            </p>
-            <div
-              className={`bg-yellow-300 text-sm font-medium text-gray-900 px-3 py-1 rounded-lg inline-block ${openSans.className}`}
-            >
-              {carouselItems[currentIndex].time}
+            <div>
+              <h3
+                className={`text-2xl font-bold text-gray-900 mb-2 ${playfairDisplay.className}`}
+              >
+                {carouselItems[currentIndex].title}
+              </h3>
+              <p
+                className={`text-secondary-main leading-relaxed mb-4 text-sm ${openSans.className}`}
+              >
+                {carouselItems[currentIndex].description}
+              </p>
+              <div
+                className={`bg-yellow-300 text-sm font-medium text-gray-900 lg:px-3 py-1 rounded-lg inline-block ${openSans.className}`}
+              >
+                {carouselItems[currentIndex].time}
+              </div>
             </div>
           </div>
 
-          {/* Thumbnails */}
-          <div className='flex flex-col md:w-[600px] lg:w-full'>
-            <div className='md:flex hidden items-center h-full md:overflow-x-scroll space-x-2'>
+          {/* Thumbnails w-[200] h-[200] md:w-[600px] */}
+          <div className='lg:flex hidden flex-col lg:w-full'>
+            <div className='lg:flex hidden items-center h-full md:overflow-x-scroll space-x-2'>
               {carouselItems.map((item, index) => (
                 <div
                   key={index}
@@ -128,15 +129,17 @@ export default function OurProgramsSection() {
                     currentIndex === index
                       ? 'border-primary-main'
                       : 'border-gray-300'
-                  } cursor-pointer w-[200] h-[200]`}
+                  } cursor-pointer `}
                   onClick={() => handleThumbnailClick(index)}
                 >
                   <Image
                     src={item.image}
                     alt={item.title}
-                    layout='fill'
-                    objectFit='cover'
-                    className='rounded-lg'
+                    // layout='fill'
+                    width='600'
+                    height='600'
+                    // objectFit='cover'
+                    className='rounded-lg w-full'
                   />
                 </div>
               ))}
